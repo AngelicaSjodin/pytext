07eab6c51701a5225d1199d90b6d244916924473
Angelica Sjödin <angelic.sjodin@gmail.com>
2024-04-24 10:03:36 +0200

    prac. read and print from json

--- json-prac.py
+++ json-prac.py
@@ -13,15 +13,25 @@
 
 # print("Data stored successfully!")
 
+# import json
+
+# # Retrieve JSON data from the file
+# with open("horoscope_data.json", "r") as file:
+#     data = json.load(file)
+
+# # Access and process the retrieved JSON data
+# date = data["data"]["date"]
+# horoscope_data = data["data"]["horoscope_data"]
+
+# # Print the retrieved data
+# print(f"Horoscope for date {date}: {horoscope_data}")
+
 import json
 
-# Retrieve JSON data from the file
 with open("horoscope_data.json", "r") as file:
     data = json.load(file)
 
-# Access and process the retrieved JSON data
-date = data["data"]["date"]
-horoscope_data = data["data"]["horoscope_data"]
 
-# Print the retrieved data
-print(f"Horoscope for date {date}: {horoscope_data}")
+# Access the dictionary values
+print(data["data"]["date"])
+print(data["data"]["horoscope_data"])
